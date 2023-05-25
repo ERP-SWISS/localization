ce68cf6a9d5eb449d85e328e82020fc68b3893eb
Aleksandr Zurnachyan <aleksandrzurnachyan@MacBook-Air-Aleksandr.local>
2023-05-25 17:10:38 +0400

    localization module

---
+++ l10n_am/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Armenian Localization',
+    'version': '1.0',
+    "category": "Accounting/Localizations/Account Charts",
+    'author': 'Erpswiss',
+    'website': 'https://www.erpswiss.com',
+    'depends': ['base', 'base_address_city', 'account'],
+    'data': [
+        'data/account_chart_template_data.xml',
+        'data/account.account.template.csv',
+        'data/res.country.state.csv',
+        'data/res.city.csv',
+        'data/res_country_data.xml',
+        "data/account_tax_group_data.xml",
+        "data/account_tax_template.xml",
+        'data/account_chart_template_configure.xml',
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
