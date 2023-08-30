0f2034ae2068519305f934621b158d921bae833f
Lyova Parsyan <lp@erpswiss.com>
2023-08-30 10:55:48 +0400

    fix logo

--- l10n_armenia/__manifest__.py
+++ l10n_armenia/__manifest__.py
@@ -17,7 +17,7 @@
         'data/account_chart_template_configure.xml',
         'views/res_partner_views.xml',
     ],
-    'images': ['static/description/Connecter Logo.png'],
+    'images': ['static/description/Connecter Logo.svg'],
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
