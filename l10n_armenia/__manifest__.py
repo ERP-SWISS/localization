f35369ac7901098df82e0f57523d46bfd949fb4a
azerpswiss <112162278+azerpswiss@users.noreply.github.com>
2023-09-13 15:29:29 +0300

    Update __manifest__.py

--- l10n_armenia/__manifest__.py
+++ l10n_armenia/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Armenian Localization',
     'version': '1.0.1',
     "category": "Accounting/Localizations",
-    'author': 'Erpswiss',
+    'author': 'ERP SWISS',
     'website': 'https://www.erpswiss.com',
     'depends': ['base','account', 'base_address_extended'],
     'data': [
