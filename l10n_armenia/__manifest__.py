991a50c1bf1d899af4ff0f86e8e73a075d4de1b0
Lyova Parsyan <lp@erpswiss.com>
2023-09-15 11:17:50 +0400

    add gif

--- l10n_armenia/__manifest__.py
+++ l10n_armenia/__manifest__.py
@@ -17,7 +17,7 @@
         'data/account_chart_template_configure.xml',
         'views/res_partner_views.xml',
     ],
-    'images': ['static/description/localization16.gif'],
+    'images': ['static/description/localization15.gif'],
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
